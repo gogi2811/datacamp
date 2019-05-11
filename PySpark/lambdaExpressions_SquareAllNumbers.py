430dba217010a368dcd870bb07d1bf8b14353ca6
“Gaurav <glotekar@instart.com>
2019-05-11 18:14:25 +0530

    lambda expression for sqauring all numbers in a list

---
+++ PySpark/lambdaExpressions_SquareAllNumbers.py
@@ -0,0 +1,9 @@
+# Print my_list in the console
+my_list = [1,2,3,4]
+print("Input list is", my_list)
+
+# Square all numbers in my_list
+squared_list_lambda = list(map(lambda x: x**2, my_list))
+
+# Print the result of the map function
+print("The squared numbers are", squared_list_lambda)
